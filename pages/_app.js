834b1c910666f8412e51687c5d5eadd121e43477
Carmen <carmenlivia.lopes@gmail.com>
2021-01-28 18:12:31 -0300

    aula 3

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+/* eslint-disable linebreak-style */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prop-types */
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
@@ -23,9 +27,9 @@ const GlobalStyle = createGlobalStyle`
     display: flex;
     flex-direction: column;
   }
-`
+`;
 
-const theme = db.theme;
+const { theme } = db;
 
 export default function App({ Component, pageProps }) {
   return (
@@ -35,5 +39,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
+/* eslint-disable func-names */
+import React from 'react';
 import styled from 'styled-components';
-import db from '../db.json';
 
+import { useRouter } from 'next/router';
+import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -19,6 +24,9 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
@@ -28,14 +36,27 @@ export default function Home() {
             <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>{db.description}</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+            }}
+            >
+              <Input
+                placeholder="Digite seu nome"
+                value={name}
+                onChange={function (infosDoEvento) {
+                  setName(infosDoEvento.target.value);
+                }
+                }
+              />
+              <Button type="submit" disabled={name.length === 0}>Jogar </Button>
+            </form>
           </Widget.Content>
         </Widget>
 
         <Widget>
           <Widget.Content>
             <h1>Quizes da Galera</h1>
-
             <p>lorem ipsum dolor sit amet...</p>
           </Widget.Content>
         </Widget>
