65c1acbc4f087e2ba7c83290e93b5849544e0be1
Carmen <carmenlivia.lopes@gmail.com>
2021-01-28 20:19:14 -0300

    add og

--- pages/_app.js
+++ pages/_app.js
@@ -36,7 +36,8 @@ export default function App({ Component, pageProps }) {
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
-        <meta property="og:url" content="https://cocomovie-quiz.carmenlopes.vercel.app/" />
+        <meta property="og:title" content="Quiz" />
+        <meta property="og:image" content="https://cocomovie-quiz.carmenlopes.vercel.app/" />
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
