6be38ee57a1617bd3c7d0b91c54ac476c6c632f7
Carmen <carmenlivia.lopes@gmail.com>
2021-01-28 20:43:50 -0300

    add og

--- pages/_app.js
+++ pages/_app.js
@@ -37,9 +37,9 @@ export default function App({ Component, pageProps }) {
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
         <meta property="og:title" content="Quiz Coco Movie" />
-        <meta property="og:image" content="blob:https://vercel.com/878f9075-d764-4664-a044-c160268aee2e" />
+        <meta property="og:image" content="https://www.nationalgeographic.com/content/dam/ngdotcom/rights-exempt/homepage/nationalgeographicog.ngsversion.1530540626597.adapt.1900.1.jpg" />
         <meta property="description" content="Fça o quiz do filme Coco" />
-        <meta property="og:url" content="https://cocomovie-quiz.carmenlopes.vercel.app/" />
+        <meta property="og:url" content="img/sreen.png" />
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
