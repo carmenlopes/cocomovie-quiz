2884b2103a4b70dd56c02b5c88f9b8b7de6d2dd0
Carmen <carmenlivia.lopes@gmail.com>
2021-01-29 00:21:54 -0300

    aula 3

--- pages/_app.js
+++ pages/_app.js
@@ -36,10 +36,6 @@ export default function App({ Component, pageProps }) {
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
-        <meta property="og:title" content="Quiz Coco Movie" />
-        <meta property="og:image" content="https://www.nationalgeographic.com/content/dam/ngdotcom/rights-exempt/homepage/nationalgeographicog.ngsversion.1530540626597.adapt.1900.1.jpg" />
-        <meta property="description" content="Fça o quiz do filme Coco" />
-        <meta property="og:url" content="img/sreen.png" />
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
